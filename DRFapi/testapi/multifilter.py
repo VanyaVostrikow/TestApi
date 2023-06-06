1d7ffcca9b08d6d479b2e27ed3a6e3b92bf0e756
Vanya Vostrikow <so.igor2013@gmail.com>
2023-06-06 16:17:22 +0700

    final_filter

--- DRFapi/testapi/multifilter.py
+++ DRFapi/testapi/multifilter.py
@@ -1,59 +1,73 @@
 from .models import CategoriesOfProducts, GroupsofProducts, Product
 from django.db.models import Q
 
-
-
 class MultiFiltering:
     def __init__(self, request, queryset):
         self.request = request
         self.queryset = queryset
+    def __del_page(self, params):
+        if type(params) == str:
+            if 'page' in params or \
+                'search' in params:
+                return None
+            else:
+                return params
+        elif type(params) ==  list:
+            out = []
+            for i in range(len(params)):
+                if  params[i].startswith('page') or \
+                    params[i].startswith('per_page') or \
+                    params[i].startswith('search'):
+                    continue
+                else:
+                    out.append(params[i])
+            return out
+
     def get__atr(self):
-        try:
-            ans = str(self.request).split('?')[1]
-            ans = ans[:-2]
             try:
-                if '&' in ans:
-                    ans=ans.split('&')
+                ans = str(self.request).split('?')[1][:-2]
+                try:
+                    if '&' in ans:
+                        ans=ans.split('&')
+                except:
+                    pass
+                ans = self.__del_page(ans)
+                if type(ans) is str:
+                    ans=ans.split('=')
+                    ans_dict = {
+                        f'{ans[0]}__startswith':ans[1]
+                    }
+                elif type(ans) is list:
+                    ans_dict = {}
+                    for i in range(len(ans)):
+                        ans_new=ans[i].split('=')
+                        ans_dict.update({
+                        f'{ans_new[0]}__startswith':ans_new[1]
+                        })   
+                elif ans == None:
+                    return None
+                return ans_dict
             except:
-                pass
-            if type(ans) is str:
-                ans=ans.split('=')
-                ans_dict = {
-                    f'{ans[0]}__startswith':ans[1]
-                }
-            elif type(ans) is list:
-                ans_dict = {}
-                for i in range(len(ans)):
-                    ans_new=ans[i].split('=')
-                    if ans_new[0] == 'page':
-                        pass
-                    ans_dict.update({
-                    f'{ans_new[0]}__startswith':ans_new[1]
-                    })   
-            print(ans_dict)     
-            return ans_dict
-        except:
-            return None
+                return None
+
     def get_filtered_queryset(self):
-        filter_attr = self.get__atr()
-        if filter_attr == None:
+        filter_dict = self.get__atr()
+        if filter_dict == None:
             data = {
             'queryset':self.queryset,
             }
             return data
         try:
-            error = CategoriesOfProducts.objects.filter(**filter_dict).exists()
-            out_queryset = CategoriesOfProducts.objects.filter(**filter_dict)
-            if error == False:
-                error = str("Nothing search")           
+            error = self.queryset.filter(**filter_dict).exists()
+            out_queryset = self.queryset.filter(**filter_dict)
+            if error == False:          
                 data = {
                     'queryset':None,
-                    'error':error
+                    'error':str("404-Not-Found") 
                 }
                 return data
             data = {
                     'queryset':out_queryset,
-                    'error':None
                 }
             return data
         except:
--- DRFapi/testapi/views.py
+++ DRFapi/testapi/views.py
@@ -1,56 +1,40 @@
-from django.shortcuts import render, get_object_or_404
-from rest_framework import exceptions
-import json
-from rest_framework.authtoken.models import Token
-from rest_framework.views import APIView
-from rest_framework import generics, viewsets, filters, mixins
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, filters, mixins
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication, get_authorization_header
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.authentication import BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .serializers import COPSerializer, GOPSerializer, PSerializer
 from .models import CategoriesOfProducts, GroupsofProducts, Product
 from .pagination import DefaultPagination
 from .deletecheck import DeleteCheck
 from .SearchFilter import MySearch
 from .multifilter import MultiFiltering
-import re
-# Create your views here.
 """
 Categories of Products UserViewset
 """
 class KeyKey(TokenAuthentication):
     keyword = 'liwest'        
 
-class COPViewSet(mixins.RetrieveModelMixin,
-                    mixins.CreateModelMixin,
-                    mixins.ListModelMixin,
-                    viewsets.GenericViewSet):
-    
+class COPViewSet(viewsets.GenericViewSet):    
     queryset = CategoriesOfProducts.objects.all()
     authentication_classes = [BasicAuthentication, KeyKey]
     permission_classes = [IsAuthenticated]
     serializer_class = COPSerializer
     pagination_class = DefaultPagination
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['seq']
-    ordering = ['seq']    
        
-    def mycreate(self, request, *args, **kwargs):
+    def mycreate(self, request):
         serializer = COPSerializer(data=request.data)
         seq = request.data.get('seq')
         serializer.is_valid(raise_exception=True)
-        serializer.check_seq(request.data.get('seq'))
+        serializer.check_seq(seq)
         serializer.save()
-        
-        return Response({'answer':serializer.data})
-    
+        return Response({'answer':serializer.data})  
 
-    def mydestroy(self, request, pk, *args, **kwargs):        #Method DELETE
+    def mydestroy(self, request, pk):        #Method DELETE
         check = DeleteCheck.COPcheck.check_delte(pk)
         if check == False:
             instance = get_object_or_404(CategoriesOfProducts, pk=pk)
             if instance:
-                ans = get_object_or_404(CategoriesOfProducts, pk=pk)
                 instance.delete()
                 return Response({'answer':GOPSerializer(data=request.data).initial_data})
             else:
@@ -85,22 +69,17 @@ class COPViewSet(mixins.RetrieveModelMixin,
             return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(list_queryset, many=True)
         return Response(serializer.data)
-
 """
 Groups of Products UserViewset
 """
-class GOPViewSet(mixins.RetrieveModelMixin,             
-                    mixins.CreateModelMixin,
-                    mixins.ListModelMixin,
-                    viewsets.GenericViewSet):
+class GOPViewSet(viewsets.GenericViewSet):
     queryset = GroupsofProducts.objects.all()
     serializer_class = GOPSerializer
     pagination_class = DefaultPagination
     filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['seq']
-    ordering = ['seq']
+    authentication_classes = [BasicAuthentication, KeyKey]
 
-    def destroy(self, request, pk, *args, **kwargs):        #method DELETE
+    def destroy(self, pk):        #method DELETE
         check = DeleteCheck.GOPcheck.check_delte(pk)
         if check == False:
             instance = get_object_or_404(GroupsofProducts, pk=pk)
@@ -112,7 +91,7 @@ class GOPViewSet(mixins.RetrieveModelMixin,
         else:
             return Response({'error':'object has source'})
         
-    def update(self, request, pk, *args, **kwargs):     #method PUT
+    def update(self, request, pk):     #method PUT
         if not pk:
             return Response({'error':'404#NOT_FOUNDED', 'message':'Method PUT not allowed'})
         try:
@@ -125,13 +104,13 @@ class GOPViewSet(mixins.RetrieveModelMixin,
         serializer.save()
         return Response({'put':serializer.validated_data})
     
-    def mycreate(self, request, *args, **kwargs):
+    def mycreate(self, request):
         serializer = GOPSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({'answer':serializer.data})
     
-    def list(self, request, *args, **kwargs): #CUSTOM GET method (hidden-field exclude)
+    def list(self, request): #CUSTOM GET method (hidden-field exclude)
         list_queryset = MultiFiltering(request,self.queryset).get_filtered_queryset()
         if list_queryset.get('queryset') == None:
                 if list_queryset.get('error') == None:
@@ -139,29 +118,24 @@ class GOPViewSet(mixins.RetrieveModelMixin,
                 error = list_queryset.get('error')
                 return Response({'error':error})
         list_queryset = list_queryset.get("queryset")
-        out_queryset = []
         page = self.paginate_queryset(list_queryset) #реализация пагинации
         if page is not None:
             serializer = self.get_serializer(page, many=True)
             return self.get_paginated_response(serializer.data)
         serializer = self.get_serializer(list_queryset, many=True)
         return Response(serializer.data)
-
 """
 Products VIEW
 """
-class PViewSet(mixins.RetrieveModelMixin,
-                    mixins.CreateModelMixin,
-                    mixins.DestroyModelMixin,
-                    mixins.ListModelMixin,
-                    viewsets.GenericViewSet):
+class PViewSet(viewsets.GenericViewSet):
     queryset = Product.objects.all()
     serializer_class = PSerializer
     pagination_class = DefaultPagination
     filter_backends = [MySearch]
+    authentication_classes = [BasicAuthentication, KeyKey]
     search_fields = ['=name']
 
-    def update(self, request, pk, *args, **kwargs):     #Method PUT
+    def update(self, request, pk):     #Method PUT
         if not pk:
             return Response({'error':'404#NOT_FOUNDED', 'message':'Method PUT not allowed'})
         try:
@@ -174,12 +148,12 @@ class PViewSet(mixins.RetrieveModelMixin,
         serializer.save()
         return Response({'put':serializer.validated_data})
     
-    def list(self, request, *args, **kwargs): #CUSTOM GET method (hidden-field exclude)
+    def list(self, request): #CUSTOM GET method (hidden-field exclude)
         list_queryset = MultiFiltering(request,self.queryset).get_filtered_queryset()
         if list_queryset.get('queryset') == None:
                 if list_queryset.get('error') == None:
                     return Response({'error404':'not founded'})
-                error = list_queryset.get('error').get('error')
+                error = list_queryset.get('error')
                 return Response({'error':error})
         list_queryset = list_queryset.get("queryset")
         out_queryset = []
@@ -205,7 +179,6 @@ class PViewSet(mixins.RetrieveModelMixin,
     def mydestroy(self, request, pk, *args, **kwargs):        #Method DELETE
         instance = get_object_or_404(Product, pk=pk)
         if instance:
-            ans = instance
             instance.delete()
             return Response({'answer':PSerializer(data=request.data).initial_data})
         else:
